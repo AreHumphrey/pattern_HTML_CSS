f74c319591133c406f2a874964064ee1d7663e0e
Dina <vinnitckaya.ds@dvfu.ru>
2025-09-20 13:03:21 +1000

    Create play.py

---
+++ play.py
@@ -0,0 +1,19 @@
+
+import pygame
+import random
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400             
+PLATFORM_WIDTH, PLATFORM_HEIGHT = 100, 15
+BALL_SIZE = 20                           
+PLATFORM_SPEED = 8                       
+BALL_SPEED = 5                          
+
+WHITE = (255, 255, 255)   
+BLACK = (0, 0, 0)        
+BLUE = (0, 100, 255)      
+RED = (255, 50, 50)       
+
+sys.exit()
